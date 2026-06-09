ef7d0b1b91ebcfc05d05c75bf7db65b7559f33dc
VichuA2 <Vivi420vishnu@gmail.com>
2026-06-09 09:53:39 +0530

    Fix scopes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,13 +4,13 @@ class Movie < ApplicationRecord
   validates :name, presence: true
   validates :release_date, presence: true
 
-  scope :search_by_name, -> (name) {
-    where("LOWER(name) LIKE ?", "%#{name.downcase}%") if name.present?
-  }
-  scope :filter_by_release_date, -> (date) {
-    where(release_date: date) if date.present?
+  scope :search_by_name, ->(name) {
+    name.present? ? where("LOWER(name) LIKE ?", "%#{name.downcase}%") : all
   }
 
+  scope :filter_by_release_date, ->(date) {
+    date.present? ? where(release_date: date) : all
+  }
   def average_rating
     reviews.average(:rating)&.round(2) || 0
   end
