dbf89db1068c11f2ae794b7d1d11450f8250ed44
VichuA2 <Vivi420vishnu@gmail.com>
2026-06-09 09:17:37 +0530

    Fix ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
-class MoviesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
-
-  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
 end
