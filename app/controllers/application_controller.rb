c7f21a259065defe154c849ac2588ed17369e029
VichuA2 <Vivi420vishnu@gmail.com>
2026-06-09 07:16:51 +0530

    Allow public movie listing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
-class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+class MoviesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
 end
